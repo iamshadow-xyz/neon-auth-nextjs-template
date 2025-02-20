ad40f5b4a92d316b48f847fa1a047eebf90dcb23
Shriidhar <shridhar.deshmukh3@gmail.com>
2025-02-20 17:04:46 +0100

    feat: copy change. fix page title and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,62 +8,51 @@ export default function Home() {
       
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start relative z-10">
         
-        <h1 className="font-title text-[48px] font-medium leading-none -tracking-[0.03em] text-white xl:text-[48px] lg:text-[32px] sm:text-[32px]">Neon Auth </h1>
-        <p className='font-light'>Effortless Authentication for Modern Apps!</p>
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started with{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/header.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <header className='flex flex-col gap-2 items-start max-w-xl'>
+          <h1 className="font-title text-[48px] font-medium leading-none -tracking-[0.03em] text-white xl:text-[48px] lg:text-[32px] sm:text-[32px]">Neon Auth </h1>
+          <p className='text-medium mt-0'>Effortless Authentication for Modern Apps!</p>
+        </header>
+        <section className='flex flex-col gap-4 mx-auto max-w-xl'>
+          <p className="text-sm text-gray-300 mb-4">
+            This example app allows users to create a user account. When a user creates an account, their data is automatically synced to your Neon database. Create an account via the sign up button above to try it yourself.
+          </p>
+          <p className="text-sm text-gray-300 mb-4">
+            Neon Auth connects <a
+              className="hover:underline hover:underline-offset-4
+              text-primary-1 hover-text-[#00e5bf]"
+              href="https://stack-auth.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >Stack Auth</a> to your Neon database, automatically synchronizing user profiles so that you own your auth data. Access your user data directly in your database environment, with no custom integration code needed.
+          </p>
+        </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://neon.tech/docs/guides/neon-auth?utm_source=neon-auth-nextjs-template&utm_campaign=neon-auth"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        
+        <ol className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-primary-1 hover-text-[#00e5bf]"
+              href="https://neon.tech/docs/guides/neon-auth?utm_source=neon-auth-nextjs-template&utm_campaign=neon-auth"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Neon Auth documentation →
+            </a>
+          </li>
+          <li className="mb-2">
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-primary-1 hover-text-[#00e5bf]"
+              href="https://docs.stack-auth.com/overview"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Stack Auth documentation →
+            </a>
+          </li>
+        </ol>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://neon.tech/docs/guides/neon-auth?utm_source=neon-auth-nextjs-template&utm_campaign=neon-auth"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://github.com/neondatabase-labs/neon-auth-demo-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Neon Auth demo
-        </a>
+        
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://neon.tech?utm_source=neon-auth-nextjs-template&utm_campaign=neon-auth"

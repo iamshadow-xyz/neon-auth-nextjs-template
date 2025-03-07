86f443de0855964b4506c254dc61cbed62c7d522
Shadow <iamshadow.xyz@gmail.com>
2025-03-08 03:10:01 +0530

    create a simple stack auth template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from 'next';
-import { StackProvider, StackTheme } from '@stackframe/stack';
-import { stackServerApp } from '../stack';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { StackProvider, StackTheme } from "@stackframe/stack";
+import { stackServerApp } from "../stack";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Neon Auth example app',
-  description: 'Neon Auth example app',
+  title: "Neon Auth example app",
+  description: "Neon Auth example app",
 };
 
 export default function RootLayout({
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.className} ${geistMono.variable} antialiased`}
       >
         <StackProvider app={stackServerApp}>
           <StackTheme>{children}</StackTheme>
---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="text-center mt-40">
+      <h1 className="txt-2xl my-5 font-bold">
+        The page you are looking for does not exist
+      </h1>
+      <link href="/">Home</link>
+    </div>
+  );
+}
